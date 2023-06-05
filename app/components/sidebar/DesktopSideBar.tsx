556b30e23ef19b3f3795275f90698db89ca1d68e
ylcN101 <yuvalcohen12234@gmail.com>
2023-06-05 17:42:19 +0300

    added conversation route

--- app/components/sidebar/DesktopSideBar.tsx
+++ app/components/sidebar/DesktopSideBar.tsx
@@ -12,7 +12,6 @@ interface DesktopSideBarProps {
 const DesktopSideBar: React.FC<DesktopSideBarProps> = ({ currentUser }) => {
   const routes = useRoutes()
   const [isOpen, setIsOpen] = useState(false)
-  console.log(currentUser)
   return (
     <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
       <nav className="mt-4 flex flex-col justify-between">
