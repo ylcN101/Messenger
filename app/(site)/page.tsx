eff2bd15f0490334b4834abfbff6ea11fe0aaa80
ylcN101 <yuvalcohen12234@gmail.com>
2023-06-12 15:05:09 +0300

    change logo

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,17 +1,13 @@
 import Image from 'next/image'
 import AuthForm from './components/AuthForm'
+import { TiMessages } from 'react-icons/ti'
+import { BsMessenger } from 'react-icons/bs'
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-full justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Image
-          alt="Logo"
-          height={48}
-          width={48}
-          className="mx-auto w-auto"
-          src="/images/logo.png"
-        />
+        <BsMessenger className="mx-auto h-12 w-auto text-gray-900" />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           Sign in to your account
         </h2>
